6074f23afd398bc795791a47d4ae338beba407c7
Mousa_Mohamed <mosapopo2016@gmail.com>
2024-11-23 07:42:16 +0200

    task3

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import Profile from './Task1.js';
-import Profile2 from './Task2.js';
-import Profile3 from './Task3.js';
-import Show from './Task4.js';
+import Gallery from "./Task1";
+import Profile1 from "./Task2";
+import Profile2 from "./Task3";
+import Custom from "./Task4";
+import PackingList1 from "./Task5";
+import PackingList2 from "./Task6";
+import DrinkList from "./Task7";
+
 
 export default function Tasks() {
     return (
-        <div>
-            <h1>Task One</h1>
-            <Profile/>
-            <h1>Task Two</h1>
-            <Profile2/>
-            <h1>Task Three</h1>
-            <Profile3/>
-            <h1>Task Four</h1>
-            <Show/>
-        </div>
-    );
+        <>
+            <div>
+                <Gallery/>
+            </div>
+            <div>
+                <Profile1/>
+            </div>
+            <div>
+                <Profile2/>
+            </div>
+            <div>
+                <Custom/>
+            </div>
+            <div>
+                <PackingList1/>
+            </div>
+            <div>
+                <PackingList2/>
+            </div>
+            <div>
+                <DrinkList/>
+            </div>
+        </>
+    )
 }
